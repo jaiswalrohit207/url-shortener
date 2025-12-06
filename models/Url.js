7b2b9480764b43e0e9b8f03055bae175f86fd3ab
Rohit Jaiswal <jaiswalrohit207@gmail.com>
2025-12-06 13:37:19 +0530

    Replace Url.js model with Sequelize MySQL version

--- models/Url.js
+++ models/Url.js
@@ -1,26 +1,50 @@
-const mongoose = require('mongoose');
+const { DataTypes } = require('sequelize');
 const { nanoid } = require('nanoid');
 
-const urlSchema = new mongoose.Schema({
-  originalUrl: {
-    type: String,
-    required: true
-  },
-  shortCode: {
-    type: String,
-    unique: true,
-    default: () => nanoid(6)
-  },
-  shortUrl: String,
-  clicks: {
-    type: Number,
-    default: 0
-  },
-  expiresAt: Date,
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+module.exports = (sequelize) => {
+  const Url = sequelize.define('Url', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    originalUrl: {
+      type: DataTypes.STRING(2048),
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    },
+    shortCode: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      unique: true,
+      defaultValue: () => nanoid(6)
+    },
+    shortUrl: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    clicks: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    expiresAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    tableName: 'urls',
+    timestamps: true
+  });
 
-module.exports = mongoose.model('Url', urlSchema);
+  return Url;
+};
